47c691939e581c158a00c630a5b5f0484a854b5f
hadi arbabi <hadimojak@gmail.com>
2026-06-10 14:42:55 +0330

    rabbit fixed

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport, RmqOptions } from '@nestjs/microservices';
+import { ClientProxyFactory, ClientProxy, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import { RabbitmqService } from './rabbitmq.service';
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: 'RABBITMQ_SERVICE',
-        inject: [ConfigService],
-        useFactory: (config: ConfigService): RmqOptions => ({
+  providers: [
+    {
+      provide: 'RABBITMQ_SERVICE',
+      inject: [ConfigService],
+      useFactory: (config: ConfigService): ClientProxy =>
+        ClientProxyFactory.create({
           transport: Transport.RMQ,
           options: {
             urls: [
               `amqp://${config.get('RABBITMQ_USER')}:${config.get('RABBITMQ_PASS')}@${config.get('RABBITMQ_HOST')}:${config.get('RABBITMQ_PORT')}`,
             ],
-            queue: config.get<string>('RBT_QUEUE_NAME'),
+            queue: config.get<string>('RABBITMQ_QUEUE_NAME'),
             queueOptions: { durable: true },
             noAck: true,
           },
         }),
-      },
-    ]),
+    },
+    RabbitmqService,
   ],
-  providers: [RabbitmqService],
   exports: [RabbitmqService],
 })
 export class RabbitmqModule {}
--- src/tasks/tasks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RabbitmqModule } from '../rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [RabbitmqModule],
-  controllers: [],
-  providers: [],
-})
-export class TasksModule {}
